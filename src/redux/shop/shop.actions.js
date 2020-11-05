197ad4102dc01833d5020e8ef11ce8ba537fe82f
ccduisburg <cemilkabakci@gmail.com>
2020-11-05 21:53:23 +0100

    Crown Shop projekt without hooks

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,30 +1,35 @@
 import ShopActionTypes from './shop.types';
-import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils';
 
-export const fetchCollectionsStart=()=>({
-    type: ShopActionTypes.FETCH_COLLECTIONS_START,
-    
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from '../../firebase/firebase.utils';
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START
 });
-export const fetchCollectionsSuccess=collectionsMap=>({
-    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
-    payload: collectionsMap
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionsMap
 });
 
-export const fetchCollectionsFailure=errorMessage=>({
-    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
-    payload: errorMessage
+export const fetchCollectionsFailure = errorMessage => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage
+});
 
-})
-
-export const fetchCollectionsStartAsync=()=>{
-return dispatch=>{
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
     const collectionRef = firestore.collection('collections');
     dispatch(fetchCollectionsStart());
-    collectionRef.get().then(
-      snapshot => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+
+    collectionRef
+      .get()
+      .then(snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
         dispatch(fetchCollectionsSuccess(collectionsMap));
-      this.setState({loading: false})
-      }).catch(error=>dispatch(fetchCollectionsFailure(error.message)))
-}
-}
+      })
+      .catch(error => dispatch(fetchCollectionsFailure(error.message)));
+  };
+};
